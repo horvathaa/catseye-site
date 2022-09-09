703693c8b6a3395a50967f0f9ff5b4c6dd5c87f1
Amber Horvath <ambear9@gmail.com>
2022-09-09 10:54:34 -0400

    fix link

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,12 @@ export default function Footer() {
         <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                 2022{' '}
-                <a href="https://amberhorvath.com/" class="hover:underline">
+                <a
+                    href="www.amberhorvath.com/"
+                    target={'_blank'}
+                    rel="noreferrer"
+                    class="hover:underline"
+                >
                     Amber Horvath
                 </a>
                 . All Rights Reserved.
